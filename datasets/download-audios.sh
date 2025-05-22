5094f7a6b52a8104b65151826e1456caec7be09a
HaikalMPPH <haikal.mpph65@gmail.com>
2025-05-23 01:30:36 +0700

    svm created, data still sparse

---
+++ datasets/download-audios.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yt-dlp -x "https://www.youtube.com/shorts/2ST1vACrRmk" --audio-format mp3 -o 1.mp3
+yt-dlp -x "https://www.youtube.com/shorts/SF9AjaBM8L8" --audio-format mp3 -o 2.mp3
+yt-dlp -x "https://www.youtube.com/shorts/DnpWZbRu3oc" --audio-format mp3 -o 3.mp3
+yt-dlp -x "https://www.youtube.com/shorts/g-iFDyTtW5E" --audio-format mp3 -o 4.mp3
+yt-dlp -x "https://www.youtube.com/shorts/cfSBe-XXQA8" --audio-format mp3 -o 5.mp3
+yt-dlp -x "https://www.youtube.com/shorts/oiSkhlNnFOI" --audio-format mp3 -o 6.mp3
+yt-dlp -x "https://www.youtube.com/shorts/QHvNTRu2tps" --audio-format mp3 -o 7.mp3
+yt-dlp -x "https://www.youtube.com/shorts/Iq21v-8CkWI" --audio-format mp3 -o 8.mp3
+yt-dlp -x "https://www.youtube.com/shorts/JbSwUFncy-k" --audio-format mp3 -o 9.mp3
+yt-dlp -x "https://www.youtube.com/shorts/IU4zgzanlVI" --audio-format mp3 -o 10.mp3
+
+# special care
+#yt-dlp -x "https://www.youtube.com/shorts/U4VjlWwNXwQ" --audio-format mp3 -o 11.mp3
